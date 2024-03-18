f31d2272e1ef325b38ec8d9682249a5462602b81
Amit Singh <royalamit7322@gmail.com>
2024-03-19 00:11:34 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
@@ -11,10 +12,11 @@ const { app, server } = require("./socket/socket");
 
 
 const PORT = process.env.PORT || 8000;
+const dirname = path.resolve()
 
 //middlewares
 dotenv.config();
-app.use(express.json()); //to parse incoming request with json payload
+app.use(express.json()); //to parsse incoming request with json payload
 app.use(cookieParser());
 // Enable CORS for all routes
 app.use(cors({
@@ -23,16 +25,20 @@ app.use(cors({
 }));
 
 //test route
-app.get("/", (req, res) => {
-  res.send("yes you are hitting home page");
-});
+// app.get("/", (req, res) => {
+//   res.send("yes you are hitting home page");
+// });
 
 //routes
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
-// app.use("/api/conversations", authRoute);
 
+app.use(express.static(path.join(dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(dirname, "frontend", "dist", "index.html"));
+});
 //starting server
 server.listen(PORT, () => {
   connetionMongoDB();
